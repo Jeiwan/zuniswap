b9396ff4326675da1ce9ebc9c093ac5804fee03d
Ivan Kuznetsov <me@jeiwan.ru>
2021-05-31 16:40:02 +0700

    Implement addLiquidity and getReserve

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Exchange {}
+contract Exchange {
+    address public tokenAddress;
+
+    constructor(address _token) {
+        require(_token != address(0), "invalid token address");
+
+        tokenAddress = _token;
+    }
+
+    function addLiquidity(uint256 _token_amount) public payable {
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(msg.sender, address(this), _token_amount);
+    }
+
+    function getReserve() public view returns (uint256) {
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+}
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -4,12 +4,34 @@ describe("Exchange", () => {
   let owner;
   let exchange;
 
-  before(async () => {
+  beforeEach(async () => {
     [owner] = await ethers.getSigners();
 
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("Token", "TKN", 1000000);
+    await token.deployed();
+
     const Exchange = await ethers.getContractFactory("Exchange");
-    exchange = await Exchange.deploy();
+    exchange = await Exchange.deploy(token.address);
     await exchange.deployed();
+  });
+
+  describe("addLiquidity", async () => {
+    it("adds liquidity", async () => {
+      await token.approve(exchange.address, 2000);
+      await exchange.addLiquidity(2000, { value: 1000 });
+
+      expect(await ethers.provider.getBalance(exchange.address)).to.equal(1000);
+      expect(await exchange.getReserve()).to.equal(2000);
+    });
+
+    it("allows zero amounts", async () => {
+      await token.approve(exchange.address, 0);
+      await exchange.addLiquidity(0, { value: 0 });
+
+      expect(await ethers.provider.getBalance(exchange.address)).to.equal(0);
+      expect(await exchange.getReserve()).to.equal(0);
+    });
   });
 
   it("is deployed", async () => {
