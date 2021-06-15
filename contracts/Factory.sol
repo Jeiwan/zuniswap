0794cfe2acd8f3a7ed99f93441dd3b7e39913b6a
Ivan Kuznetsov <me@jeiwan.ru>
2021-06-15 15:19:32 +0700

    Implement Factory

---
+++ contracts/Factory.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./Exchange.sol";
+
+contract Factory {
+    mapping(address => address) public tokenToExchange;
+
+    function createExchange(address _tokenAddress) public returns (address) {
+        require(_tokenAddress != address(0), "invalid token address");
+        require(
+            tokenToExchange[_tokenAddress] == address(0),
+            "exchange already exists"
+        );
+
+        Exchange exchange = new Exchange(_tokenAddress);
+        tokenToExchange[_tokenAddress] = address(exchange);
+
+        return address(exchange);
+    }
+}
