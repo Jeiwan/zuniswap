59c57bf9a1f1425a54303e57807a3d6bcbc1a0ca
Ivan Kuznetsov <me@jeiwan.ru>
2021-08-05 14:54:22 +0300

    Fix the deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,12 @@
 const hre = require("hardhat");
 
 async function main() {
+  const Token = await ethers.getContractFactory("Token");
+  token = await Token.deploy("Token", "TKN", (10 ** 18).toString());
+  await token.deployed();
+
   const Exchange = await hre.ethers.getContractFactory("Exchange");
-  const greeter = await Exchange.deploy();
+  const greeter = await Exchange.deploy(token.address);
 
   await greeter.deployed();
 
